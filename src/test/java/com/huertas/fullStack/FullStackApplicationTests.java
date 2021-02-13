60b4696391f113ca01d75421a9fa61351633eb81
Andhurt <andreshuertasgon@gmail.com>
2021-02-13 16:00:07 -0500

    Maqueta del proyecto

---
+++ src/test/java/com/huertas/fullStack/FullStackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huertas.fullStack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackApplicationTests {
+
+	/*@Test
+	void contextLoads() {
+	}*/
+
+}
