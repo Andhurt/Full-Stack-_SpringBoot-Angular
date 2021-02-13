8eee394506a80eb147df9de793efef58d5864430
Andhurt <andreshuertasgon@gmail.com>
2021-02-13 17:48:14 -0500

    Spring Query Methods

--- src/main/java/com/huertas/fullStack/Repository/EmployeeRepository.java
+++ src/main/java/com/huertas/fullStack/Repository/EmployeeRepository.java
@@ -3,5 +3,10 @@ package com.huertas.fullStack.Repository;
 import com.huertas.fullStack.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long idEmployee);
+
+    Optional findEmployeeById(Long id);
 }
--- src/main/java/com/huertas/fullStack/Service/EmployeeService.java
+++ src/main/java/com/huertas/fullStack/Service/EmployeeService.java
@@ -5,6 +5,7 @@ import com.huertas.fullStack.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,10 +18,49 @@ public class EmployeeService {
         this.employeeRepo = employeeRepo;
     }
 
+    /**
+     * Save the employee passed as parameter
+     * @param employee
+     * @return employee saved
+     */
     public Employee addEmployee(Employee employee){
         employee.setEmployeeCode(UUID.randomUUID().toString());
         return employeeRepo.save(employee);
     }
 
+    /**
+     * Find all the employees in the database
+     * @return the list of all employees in the databse.
+     */
+    public List<Employee> findAllEmployee(){
+        return employeeRepo.findAll();
+    }
+
+    /**
+     * Update the employee passed as parameter
+     * @param employe
+     * @return employee updated
+     */
+    public Employee updateEmployee(Employee employe){
+        return employeeRepo.save(employe);
+    }
+
+    /**
+     * Return the employee finded by ID passed as parameter
+     * @param id of the employe to find.
+     * @return employee with the id passed as parameter
+     */
+    public Employee findEmployeeById(Long id){
+        return employeeRepo.findEmployeeById(id)
+                    .orElseThrow(()-> new UserNotFoundException("User by id "+id+" was not found"));
+    }
+
+    /**
+     * Delete an employee
+     * @param idEmployee of the employee to delete.
+     */
+    public void deleteEmployee(Long idEmployee){
+       employeeRepo.deleteEmployeeById(idEmployee);
+    }
 
 }
